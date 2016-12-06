0f16d6b56a876d628ce8996db65fa65a08a8dd1c
dayatz <dayat.py@gmail.com>
2016-12-06 21:30:25 +0800

    exp

---
+++ assets/script.js
@@ -0,0 +1,21 @@
+document.querySelector('#html').addEventListener('mdl-componentupgraded', function() {
+    this.MaterialProgress.setProgress(70);
+});
+document.querySelector('#js').addEventListener('mdl-componentupgraded', function() {
+    this.MaterialProgress.setProgress(75);
+});
+document.querySelector('#py').addEventListener('mdl-componentupgraded', function() {
+    this.MaterialProgress.setProgress(80);
+});
+document.querySelector('#dj').addEventListener('mdl-componentupgraded', function() {
+    this.MaterialProgress.setProgress(90);
+});
+document.querySelector('#djr').addEventListener('mdl-componentupgraded', function() {
+    this.MaterialProgress.setProgress(95);
+});
+document.querySelector('#react').addEventListener('mdl-componentupgraded', function() {
+    this.MaterialProgress.setProgress(80);
+});
+document.querySelector('#react-native').addEventListener('mdl-componentupgraded', function() {
+    this.MaterialProgress.setProgress(60);
+});
